9ef655297c232aa246a62ec70fcaa30cc28b2a2b
fbansept <bansept.franck@gmail.com>
2026-02-11 10:43:50 +0100

    commit 3

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -7,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './product-details.scss',
 })
 export class ProductDetails {
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+  httpClient = inject(HttpClient);
 
-  
+  product : WritableSignal<Product | null> = signal(null)
 
+  ngOnInit() {
+    this.route.params.subscribe((parametres) => {
+      if (!Number.isNaN(+parametres['id'])) {
+
+        this.httpClient
+          .get<Product>(`https://dummyjson.com/products/${parametres['id']}`)
+          .subscribe(product => this.product.set(product));
+
+      } else {
+        //le parametre n'est pas un nombre
+        this.router.navigate(['/404']);
+      }
+    });
+  }
 }
